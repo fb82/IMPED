1b973417052220dbdcd9456fb53231b9c9c3d7a2
bellavista <bellavista@phoenix-marie>
2025-01-23 16:06:56 +0100

    Update base_tools.py, utils.py, and viz2d.py

--- src/base_tools.py
+++ src/base_tools.py
@@ -14,9 +14,14 @@ import numpy as np
 import hz.hz as hz
 from PIL import Image
 import poselib
+
 import matplotlib.pyplot as plt
+from matplotlib import colormaps
+import plot.viz2d as viz
+import plot.utils as viz_utils
  
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+pipe_color = ['red', 'blue', 'lime', 'fuchsia', 'yellow']
 show_progress = True
 
 def go_iter(to_iter, msg='', active=True, params=None):
@@ -374,7 +379,7 @@ def set_args(id_string, args, args_):
     return id_string, args_    
 
 
-class sift_module:
+class dog_module:
     def __init__(self, **args):
         self.single_image = True
         self.pipeliner = False                
@@ -492,7 +497,7 @@ class show_kpts_module:
             'prepend_pair': False,
             'ext': '.jpg',
             'force': False,
-            'mask_idx': None, # None: all single image, -1: all both images, []: filtered both images
+            'mask_idx': None, # None: all single image, -1: all both images, list: filtered both images
             'params': [{'color': 'r', 'linewidth': 1, 'draw_ori': True}, {'color': 'g', 'linewidth': 1, 'draw_ori': True}],
         }
         
@@ -557,6 +562,111 @@ class show_kpts_module:
                 plt.savefig(new_img, dpi=150, bbox_inches='tight')
                 plt.close(fig)
 
+        return {}
+
+
+class show_matches_module:
+    def __init__(self, **args):
+        self.single_image = False
+        self.pipeliner = False        
+        self.pass_through = True
+
+        self.args = {
+            'id_more': '',
+            'img_prefix': '',
+            'img_suffix': '',
+            'cache_path': 'show_imgs',
+            'prepend_pair': True,
+            'ext': '.jpg',
+            'force': False,
+            'mask_idx': [1], # -1: all, [1]: inliers, [0]: outliers, [0, 1]: outlier and inliers with differen colors
+            'fig_min_size': 960,
+            'fig_max_size': 1280, 
+            'params': [{'color': [1, 0, 0]}, {'color': [0, 0, 1]}],
+        }
+        
+        self.id_string, self.args = set_args('show_matches' , args, self.args)
+
+                
+    def get_id(self): 
+        return self.id_string
+
+    
+    def run(self, **args):         
+        im0 = os.path.splitext(os.path.split(args['img'][0])[1])[0]
+        im1 = os.path.splitext(os.path.split(args['img'][1])[1])[0]
+
+        if self.args['prepend_pair']:            
+            cache_path = os.path.join(self.args['cache_path'], im0 + '_' + im1)
+        else:
+            cache_path = self.args['cache_path']
+                
+        new_img = os.path.join(cache_path, self.args['img_prefix'] + im0 + '_' + im1 + self.args['img_suffix'] + self.args['ext'])
+    
+        if not os.path.isfile(new_img) or self.args['force']:
+            os.makedirs(self.args['cache_path'], exist_ok=True)
+
+            fig = plt.figure()    
+            img0 = viz_utils.load_image(args['img'][0])
+            img1 = viz_utils.load_image(args['img'][1])
+            fig, axes = viz.plot_images([img0, img1], fig_num=fig.number)              
+
+            if 'm_idx' in args:
+                if self.args['mask_idx'] == -1:
+                    mask_idx = -1
+                    params = self.args['params'][-1]
+
+                    m_idx = args['m_idx']
+                    pt1 = args['kp'][0][m_idx[:, 0]]
+                    pt2 = args['kp'][1][m_idx[:, 1]]
+
+                    if pt1.shape[0] > 0:
+                        viz.plot_matches(pt1, pt2, color=self.args['params'][0]['color'], lw=0.2, ps=6, a=0.3, axes=axes, fig_num=fig.number)
+                else:
+                    if not isinstance(self.args['mask_idx'], list): self.args['mask_idx'] = [self.args['mask_idx']]                    
+                    mask_idx = self.args['mask_idx']
+                    params = self.args['params']
+
+                    m_mask = args['m_mask']
+                    m_sum = torch.tensor([(m_mask == i).sum().item() for i in mask_idx], device=device)
+                    idx = torch.argsort(m_sum, descending=True)
+
+                    mask_idx = [mask_idx[i] for i in idx]
+                    params = [params[i] for i in idx]
+                    
+                    for i in mask_idx:
+                        
+                        m_idx = args['m_idx'][args['m_mask'] == i]
+                        if m_idx.shape[0] < 1: continue                        
+
+                        pt1 = args['kp'][0][m_idx[:, 0]]
+                        pt2 = args['kp'][1][m_idx[:, 1]]
+
+                        viz.plot_matches(pt1, pt2, color=self.args['params'][i]['color'], lw=0.2, ps=6, a=0.3, axes=axes, fig_num=fig.number)
+            
+            fig_dpi = fig.get_dpi()
+            fig_sz = [fig.get_figwidth() * fig_dpi, fig.get_figheight() * fig_dpi]
+        
+            fig_min_size = self.args['fig_min_size']
+            fig_max_size = self.args['fig_max_size']
+        
+            fig_cz = min(fig_sz)
+            if fig_cz < fig_min_size:
+                fig_sz[0] = fig_sz[0] / fig_cz * fig_min_size
+                fig_sz[1] = fig_sz[1] / fig_cz * fig_min_size
+        
+            fig_cz = max(fig_sz)
+            if fig_cz > fig_min_size:
+                fig_sz[0] = fig_sz[0] / fig_cz * fig_max_size
+                fig_sz[1] = fig_sz[1] / fig_cz * fig_max_size
+                
+            fig.set_size_inches(fig_sz[0] / fig_dpi, fig_sz[1]  / fig_dpi)
+        
+            viz.save_plot(new_img, fig)
+            viz.clear_plot(fig)
+                        
+            plt.close(fig)
+        
         return {}
 
 
@@ -651,7 +761,7 @@ class deep_descriptor_module:
         return {'desc': desc}
 
 
-class sift_descriptor_module:
+class sift_module:
     def __init__(self, **args):
         self.single_image = True
         self.pipeliner = False        
@@ -1389,15 +1499,18 @@ class loftr_module:
 if __name__ == '__main__':    
     with torch.inference_mode():     
         pipeline = [
-            keynet_module(),
-            show_kpts_module(id_more='first', prepend_pair=True),
+            dog_module(),
+            show_kpts_module(id_more='first', prepend_pair=False),
             deep_patch_module(),
             show_kpts_module(id_more='second', img_prefix='orinet_affnet_', prepend_pair=True),
             deep_descriptor_module(),
             smnn_module(),
             poselib_module(),
-            show_kpts_module(id_more='third', img_prefix='ransac_b_', prepend_pair=True, mask_idx=[0, 1]),
-         ]
+            show_kpts_module(id_more='third', img_prefix='ransac_', prepend_pair=True, mask_idx=[0, 1]),
+            show_matches_module(id_more='forth', img_prefix='matches_', mask_idx=[1, 0]),
+            show_matches_module(id_more='fifth', img_prefix='matches_inliers_', mask_idx=[1]),
+            show_matches_module(id_more='sixth', img_prefix='matches_all_', mask_idx=-1),
+        ]
         
         imgs = '/media/bellavista/Dati2/colmap_working/villa_giulia2/imgs'
         run_pairs(pipeline, imgs)
