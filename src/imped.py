b022f2fe0a26ae8d079b12d2ab262266e6d067ec
bellavista <bellavista@phoenix-marie>
2025-02-13 19:30:28 +0100

    Update .gitmodules, acne_custom.py, config.py, and 26 more files

--- src/imped.py
+++ src/imped.py
@@ -25,6 +25,11 @@ import argparse
 import lpm.LPM as lpm
 from romatch import roma_outdoor, roma_indoor, tiny_roma_v1_outdoor
 import math
+import gms.python.gms_matcher as gms
+import adalam.adalam.adalam as adalam
+import fcgnn.fcgnn as fcgnn
+import copy
+import wget
 
 import colmap_db.database as coldb
 import matplotlib.pyplot as plt
@@ -32,7 +37,7 @@ import plot.viz2d as viz
 import plot.utils as viz_utils
  
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-# device = 'cpu'
+# device = torch.device('cpu')
 pipe_color = ['red', 'blue', 'lime', 'fuchsia', 'yellow']
 show_progress = True
 enable_quadtree = False
@@ -4049,8 +4054,6 @@ class lpm_module:
         return {'m_mask': mm}
 
 
-import gms.python.gms_matcher as gms
-
 class gms_module:
     class gms_matcher_custom(gms.GmsMatcher):
         def __init__(self, kp1, kp2, m12):
@@ -4176,6 +4179,229 @@ class gms_module:
         mm[aux] = mask
         
         return {'m_mask': mm}
+
+
+class adalam_module:
+    class adalamfilter_custom(adalam.AdalamFilter):
+        def __init__(self, custom_config=None):         
+            super().__init__(custom_config=custom_config)
+            
+
+        def match_and_filter(self, k1, k2, im1shape=None, im2shape=None, o1=None, o2=None, s1=None, s2=None, putative_matches=None, scores=None, mnn=None):    
+            if s1 is None or s2 is None:
+                if self.config['scale_rate_threshold'] is not None:
+                    raise AttributeError("Current configuration considers keypoint scales for filtering, but scales have not been provided.\n"
+                                         "Please either provide scales or set 'scale_rate_threshold' to None to disable scale filtering")
+            if o1 is None or o2 is None:
+                if self.config['orientation_difference_threshold'] is not None:
+                    raise AttributeError(
+                        "Current configuration considers keypoint orientations for filtering, but orientations have not been provided.\n"
+                        "Please either provide orientations or set 'orientation_difference_threshold' to None to disable orientations filtering")
+       
+            if not self.config['force_seed_mnn']:
+                mnn = None
+    
+            return self.filter_matches(k1, k2, putative_matches, scores, mnn, im1shape, im2shape, o1, o2, s1, s2)
+        
+
+    def __init__(self, **args):       
+        self.single_image = False    
+        self.pipeliner = False     
+        self.pass_through = False
+                
+        self.args = {
+            'id_more': '',
+            'adalam_params': {
+                    'area_ratio': 100,
+                    'search_expansion': 4,
+                    'ransac_iters': 128,
+                    'min_inliers': 6,
+                    'min_confidence': 200,
+                    'orientation_difference_threshold': 30, 
+                    'scale_rate_threshold': 1.5, 
+                    'detected_scale_rate_threshold': 5, 
+                    'refit': True, 
+                    'force_seed_mnn': False,
+                    'device': device,
+                    'th': 0.8 **2,
+                },   
+            }
+        
+        self.id_string, self.args = set_args('adalam', args, self.args)        
+        if self.args['adalam_params']['device'] is None:
+            self.args['adalam_params']['device'] = device
+        
+        self.matcher = self.adalamfilter_custom(self.args['adalam_params'])
+
+
+    def get_id(self): 
+        return self.id_string
+
+    
+    def finalize(self):
+        return
+
+        
+    def run(self, **args):  
+        sz1 = Image.open(args['img'][0]).size
+        sz2 = Image.open(args['img'][1]).size
+
+        k1 = args['kp'][0]
+        k2 = args['kp'][1]
+        
+        lafs1 = homo2laf(args['kp'][0], args['kH'][0])
+        lafs2 = homo2laf(args['kp'][1], args['kH'][1])
+        
+        kp1 = opencv_kpts_from_laf(lafs1)
+        kp2 = opencv_kpts_from_laf(lafs2)
+
+        o1 = torch.tensor([kp.angle for kp in kp1], device=device)
+        o2 = torch.tensor([kp.angle for kp in kp2], device=device)
+
+        s1 = torch.tensor([kp.size for kp in kp1], device=device)
+        s2 = torch.tensor([kp.size for kp in kp2], device=device)
+
+        mi = args['m_idx']
+        mm = args['m_mask']
+        mv = args['m_val']
+
+        m12 = mi[mm]
+        scores = mv[mm]
+        
+        k1 = k1[m12[:, 0]]
+        k2 = k2[m12[:, 1]]
+
+        s1 = s1[m12[:, 0]]
+        s2 = s2[m12[:, 1]]
+
+        o1 = o1[m12[:, 0]]
+        o2 = o2[m12[:, 1]]
+        
+        puta_match = torch.arange(0, m12.shape[0], device=device)
+        mask = self.matcher.match_and_filter(k1, k2, im1shape=sz1, im2shape=sz2, o1=o1, o2=o2, s1=s1, s2=s2, putative_matches=puta_match, scores=scores, mnn=None)
+        
+        mask_aux = torch.zeros(m12.shape[0], device=device, dtype=torch.bool)
+        mask_aux[mask[:, 0]] = True
+         
+        aux = mm.clone()
+        mm[aux] = mask_aux
+        
+        return {'m_mask': mm}
+
+
+def download_fcgnn(weight_path='../weights/fcgnn'):
+    file = 'fcgnn.model'    
+    url = "https://github.com/xuy123456/fcgnn/releases/download/v0/fcgnn.model"
+
+    os.makedirs(weight_path, exist_ok=True)   
+
+    file_to_download = os.path.join(weight_path, file)    
+    if not os.path.isfile(file_to_download):    
+        wget.download(url, file_to_download)
+
+
+class fcgnn_module:
+    class fcgnn_custom(fcgnn.GNN):
+        def __init__(self, depth=9):
+            torch.nn.Module.__init__(self)
+    
+            in_dim, r, self.n = 256, 20, 8
+            AttnModule = fcgnn.Attention(in_dim=in_dim, num_heads=8)
+            self.layers = torch.nn.ModuleList([copy.deepcopy(AttnModule) for _ in range(depth)])
+    
+            self.embd_p = torch.nn.Sequential(fcgnn.BasicBlock(self.n*4, in_dim, torch.nn.Tanh()))
+            self.embd_f = torch.nn.Sequential(fcgnn.BasicBlock((2*r+1)**2*2, 3*in_dim), torch.nn.LayerNorm(3*in_dim), fcgnn.BasicBlock(3*in_dim, in_dim))
+    
+            self.extract = fcgnn.ExtractPatch(r)
+    
+            self.mlp_s = torch.nn.Sequential(fcgnn.OutBlock(in_dim, 1), torch.nn.Sigmoid())
+            self.mlp_o = torch.nn.Sequential(fcgnn.OutBlock(in_dim, 2))
+                
+            download_fcgnn()
+            local_path = '../weights/fcgnn/fcgnn.model'    
+            self.load_state_dict(torch.load(local_path, map_location='cpu', weights_only=False)) 
+
+                
+        def optimize_matches_custom(self, img1, img2, matches, thd=0.999, min_matches=10):
+    
+            if len(matches.shape) == 2:
+                matches = matches.unsqueeze(0)
+    
+            matches = matches.round()
+            offsets, scores = self.forward(img1, img2, matches)
+            matches[:, :, 2:] = matches[:, :, 2:] + offsets[:, :, [1, 0]]
+            mask = scores[0] > thd
+            
+            if mask.sum() < min_matches:
+                mask_i = scores[0].topk(k=min(matches.shape[1], min_matches))
+                mask[mask_i[1]] = True
+                    
+            return matches[0].detach(), mask                
+                
+
+    def __init__(self, **args):       
+        self.single_image = False    
+        self.pipeliner = False     
+        self.pass_through = False
+                
+        self.args = {
+            'id_more': '',
+            'thd': 0.999,
+            'min_matches': 10,
+            }
+        
+        self.id_string, self.args = set_args('fcgnn', args, self.args)     
+        self.fcgnn_refiner = self.fcgnn_custom().to(device)        
+
+
+    def get_id(self): 
+        return self.id_string
+
+    
+    def finalize(self):
+        return
+
+        
+    def run(self, **args): 
+        img1 = cv2.imread(args['img'][0], cv2.IMREAD_GRAYSCALE)
+        img2 = cv2.imread(args['img'][1], cv2.IMREAD_GRAYSCALE)
+        
+        img1_ = torch.tensor(img1.astype('float32') / 255.)[None, None].to(device)
+        img2_ = torch.tensor(img2.astype('float32') / 255.)[None, None].to(device)        
+        
+        mi = args['m_idx']
+        mm = args['m_mask']
+
+        m12 = mi[mm]
+
+        k1 = args['kp'][0]
+        k2 = args['kp'][1]
+        
+        k1 = k1[m12[:, 0]]
+        k2 = k2[m12[:, 1]]
+        
+        matches = torch.hstack((k1, k2))
+
+        matches_refined, mask = self.fcgnn_refiner.optimize_matches_custom(img1_, img2_, matches, thd=self.args['thd'], min_matches=self.args['min_matches']) 
+
+        aux = mm.clone()
+        mm[aux] = mask
+        
+        k1 = matches_refined[mask, :2]
+        k2 = matches_refined[mask, 2:]
+
+        kp1 = args['kp'][0]
+        kp2 = args['kp'][1]
+
+        m12 = mi[mm]
+
+        kp1[m12[:, 0]] = k1        
+        kp2[m12[:, 1]] = k2        
+        
+        kp = [kp1, kp2]
+        
+        # masked keypoints are refined too but the patch shape remain the same!
+        return {'kp': kp, 'm_mask': mm}
 
 
 if __name__ == '__main__':    
@@ -4323,7 +4549,7 @@ if __name__ == '__main__':
             deep_descriptor_module(),
             smnn_module(),
             show_matches_module(id_more='first', img_prefix='matches_', mask_idx=[1, 0]),
-            gms_module(),
+            fcgnn_module(),
             show_matches_module(id_more='second', img_prefix='matches_after_filter_', mask_idx=[1, 0]),
             magsac_module(),
             show_matches_module(id_more='third', img_prefix='matches_final_', mask_idx=[1, 0]),
