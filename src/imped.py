484fa4fe37f728f6180d98c028a5b0b1bb946bb0
WaRLoCK <warlock@honey-gold>
2025-07-28 02:07:10 +0200

    Update imped.py, miho.py, miho_other.py, and 2 more files

--- src/imped.py
+++ src/imped.py
@@ -25,10 +25,9 @@ import copy
 import wget
 import pycolmap
 import scipy
-import miho.src.miho as mop_miho
-import miho.src.miho_other as mop
-import miho.src.ncc as ncc
-from scipy.spatial import Delaunay
+import miho_next.miho as mop_miho
+import miho_next.miho_other as mop
+import miho_next.ncc as ncc
 
 import matplotlib.pyplot as plt
 from matplotlib import colormaps
@@ -6919,9 +6918,13 @@ class mop_miho_ncc_module:
                 r = self.args['patch_radius']
                 S = torch.tensor([[1/r, 0, 0],[0, 1/r, 0],[0, 0, 1]], device=device).unsqueeze(0).repeat(p1.shape[0], 1, 1)
                                 
-                Ha = torch.stack([Hs_mop_[i][0] for i in range(len(Hs_mop_))], dim=0)
-                Hb = torch.stack([Hs_mop_[i][1] for i in range(len(Hs_mop_))], dim=0)
-                                
+                if self.args['miho']:                
+                    Ha = torch.stack([Hs_mop_[i][0] for i in range(len(Hs_mop_))], dim=0)
+                    Hb = torch.stack([Hs_mop_[i][1] for i in range(len(Hs_mop_))], dim=0)
+                else:
+                    Ha = torch.stack([Hs_mop_[i][0][0] for i in range(len(Hs_mop_))], dim=0)
+                    Hb = Ha.inverse()
+                    
                 H1 = Ha[Hidx[Hidx > -1]]
                 H2 = Hb[Hidx[Hidx > -1]]
                 
@@ -7951,6 +7954,148 @@ class dtm_module:
         return {'m_mask': torch.tensor(dtm_mask <= 0, dtype=torch.bool, device=device)}        
 
 
+def decompose_H(H, ret_err=False):
+    # H = torch.rand((5, 3, 3), device=device)
+    
+    v = H[:, -1, -1]
+    V = H[:, -1, :2]
+    T = H[:, :2, -1] / v.unsqueeze(-1)
+    W = H[:, :2, :2] - T.unsqueeze(-1).bmm(V.unsqueeze(1))
+    [R_, K_] = torch.linalg.qr(W)
+    M_ = torch.eye(2, device=device).unsqueeze(0).repeat(H.shape[0], 1, 1)
+    
+    # the determinant inversion can be obtained by multiplying a row or a column by -1
+    # this is done for instance by the orthogonal matrix [1 0; 0 -1] (reflection matrix)
+    # notice that [1 0; 0 -1]*[1 0; 0 -1]=eye(2);
+    
+    # det sign
+    t = K_.det().sign() < 0
+    # this change the sign of det
+    K_[t] = torch.tensor([[1., 0.], [0., -1.]], device=device) @ K_[t] 
+    R_[t] = R_[t] @ torch.tensor([[1., 0.], [0., -1.]], device=device)
+    
+    # det sign    
+    t = R_.det().sign() < 0    
+    # this change the sign of det
+    R_[t] = torch.tensor([[1., 0.], [0., -1.]], device=device) @ R_[t]
+    # this is the inverse to nullify the total effect
+    M_[t] = M_[t] @ torch.tensor([[1., 0.], [0., -1.]], device=device)
+    
+    s = R_.bmm(K_).det().abs() ** 0.5
+    K = K_ / (K_.det().abs() ** 0.5).unsqueeze(-1).unsqueeze(-1)
+    R = R_ / (R_.det().abs() ** 0.5).unsqueeze(-1).unsqueeze(-1)
+    M = M_
+    
+    # projective
+    H_p = torch.eye(3, device=device).unsqueeze(0).repeat(H.shape[0], 1, 1)
+    H_p[:, -1, :2] = V
+    H_p[:, -1, -1] = v
+     
+    # affine
+    H_a = torch.eye(3, device=device).unsqueeze(0).repeat(H.shape[0], 1, 1)
+    H_a[:, :2, :2] = K
+    
+    # rotation
+    H_r = torch.eye(3, device=device).unsqueeze(0).repeat(H.shape[0], 1, 1)
+    H_r[:, :2, :2] = R
+    
+    # reflection
+    H_m = torch.eye(3, device=device).unsqueeze(0).repeat(H.shape[0], 1, 1)
+    H_m[:, :2, :2] = M
+    
+    # scale
+    H_s = torch.eye(3, device=device).unsqueeze(0).repeat(H.shape[0], 1, 1)
+    H_s[:, 0, 0] = s
+    H_s[:, 1, 1] = s
+    
+    # translation
+    H_t = torch.eye(3, device=device).unsqueeze(0).repeat(H.shape[0], 1, 1)
+    H_t[:, :2, -1] = T
+    
+    if ret_err:    
+        err = (H - H_t.bmm(H_s.bmm(H_m.bmm(H_r.bmm(H_a.bmm(H_p)))))).abs().sum(dim=(1, 2))
+    else:
+        err = None
+        
+    return H_t, H_s, H_m, H_r, H_a, H_p, err
+
+
+def decompose_H_other(H, ret_err=False):
+    # H = torch.rand((5, 3, 3), device=device)
+    
+    A_ = H[:, :2, :2]
+    a_ = H[:, -1, :2]
+    t_ = H[:, :2, -1]
+    a = H[:, -1, -1]
+    
+    # RQ decomposition
+    P = torch.tensor([[0., 1.], [1., 0.]], device=device)
+    R_, K_ = torch.linalg.qr(A_.permute(0, 2, 1) @ P)
+    K_ = P @ K_.permute(0, 2, 1) @ P
+    R_ = P @ R_.permute(0, 2, 1)
+        
+    M_ = torch.eye(2, device=device).unsqueeze(0).repeat(H.shape[0], 1, 1)
+
+    # the determinant inversion can be obtained by multiplying a row or a column by -1
+    # this is done for instance by the orthogonal matrix [1 0; 0 -1] (reflection matrix)
+    # notice that [1 0; 0 -1]*[1 0; 0 -1]=eye(2);
+
+    # det sign
+    t = K_.det().sign() < 0
+    # this change the sign of det
+    K_[t] =  K_[t] @ torch.tensor([[1., 0.], [0., -1.]], device=device)
+    R_[t] = torch.tensor([[1., 0.], [0., -1.]], device=device) @ R_[t]
+
+    # det sign    
+    t = R_.det().sign() < 0    
+    # this change the sign of det
+    R_[t] = torch.tensor([[1., 0.], [0., -1.]], device=device) @ R_[t]
+    # this is the inverse to nullify the total effect
+    M_[t] = M_[t] @ torch.tensor([[1., 0.], [0., -1.]], device=device)
+
+    s = R_.bmm(K_).det().abs() ** 0.5
+    K = K_ / (K_.det().abs() ** 0.5).unsqueeze(-1).unsqueeze(-1)
+    R = R_ / (R_.det().abs() ** 0.5).unsqueeze(-1).unsqueeze(-1)
+    M = M_
+
+    V = torch.linalg.inv(A_.permute(0, 2, 1)).bmm(a_.unsqueeze(-1))
+    T = torch.linalg.inv(K).bmm(t_.unsqueeze(-1))
+    v = a - V.permute(0, 2, 1).bmm(t_.unsqueeze(-1)).squeeze(-1).squeeze(-1)
+
+    # projective
+    H_p = torch.eye(3, device=device).unsqueeze(0).repeat(H.shape[0], 1, 1)
+    H_p[:, -1, :2] = V.squeeze(-1)
+    H_p[:, -1, -1] = v
+     
+    # affine
+    H_a = torch.eye(3, device=device).unsqueeze(0).repeat(H.shape[0], 1, 1)
+    H_a[:, :2, :2] = K
+    
+    # rotation
+    H_r = torch.eye(3, device=device).unsqueeze(0).repeat(H.shape[0], 1, 1)
+    H_r[:, :2, :2] = R
+    
+    # reflection
+    H_m = torch.eye(3, device=device).unsqueeze(0).repeat(H.shape[0], 1, 1)
+    H_m[:, :2, :2] = M
+    
+    # scale
+    H_s = torch.eye(3, device=device).unsqueeze(0).repeat(H.shape[0], 1, 1)
+    H_s[:, 0, 0] = s
+    H_s[:, 1, 1] = s
+    
+    # translation
+    H_t = torch.eye(3, device=device).unsqueeze(0).repeat(H.shape[0], 1, 1)
+    H_t[:, :2, -1] = T.squeeze(-1)
+    
+    if ret_err:    
+        err = (H - H_p.bmm(H_a.bmm(H_t.bmm(H_s.bmm(H_m.bmm(H_r)))))).abs().sum(dim=(1, 2))
+    else:
+        err = None
+        
+    return H_p, H_a, H_t, H_s, H_m, H_r, err
+
+
 if __name__ == '__main__':       
     with torch.inference_mode():         
 #       pipeline = [
@@ -8511,5 +8656,23 @@ if __name__ == '__main__':
 #       ]
 #       imgs = '../data/ET'
 #       run_pairs(pipeline, imgs) 
+
+        pipeline = [
+            dog_module(),
+            patch_module(),
+            deep_descriptor_module(),
+            smnn_module(),
+            show_matches_module(id_more='first', img_prefix='matches_', mask_idx=[1, 0]),
+#           show_kpts_module(id_more='first', img_prefix='patches_', mask_idx=[1, 0], prepend_pair=True),
+            mop_miho_ncc_module(ncc=False),
+            show_matches_module(id_more='second', img_prefix='matches_after_filter_', mask_idx=[1, 0]),
+#           show_kpts_module(id_more='second', img_prefix='patches_after_filter_', mask_idx=[1, 0], prepend_pair=True),
+#           show_patches_module(id_more='first', img_prefix='block_patches_', prepend_pair=True),
+#           magsac_module(),
+#           show_matches_module(id_more='third', img_prefix='matches_final_', mask_idx=[1, 0]),
+#           show_kpts_module(id_more='third', img_prefix='patches_after_final_', mask_idx=[1, 0], prepend_pair=True),
+        ]
+        imgs = '../data/ET'
+        run_pairs(pipeline, imgs) 
 
         print('doh!')
